192f6891656e0c59d759e7d1128f6c0669315abb
vinaykumarsingh <vs3486@gmail.com>
2021-08-03 09:26:43 +0530

    javascript learn 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+// variable 
+// var var1:string = 'vinay'
+// let let1: string = 'singh'
+// constru
+// const
+
   title = 'Training';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    debugger
+    var var1 = 'vinay'
+    let let1 = 'singh'
+    let1 = 'vinay'
+    const const1  = 'Angular learn'
+
+    let d1 = 3;
+    let d2 = 5;
+    console.log('summing data===>',d1 + d2)
+    console.log('is same ',var1 === let1)
+
+  }
 }

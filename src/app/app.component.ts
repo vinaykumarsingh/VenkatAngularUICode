db9a5edbe94e2e68eafcc429c7d3eded9d1c4ecb
vinaykumarsingh <vs3486@gmail.com>
2021-08-05 09:25:55 +0530

    javascript- tut2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,5 +30,45 @@ export class AppComponent implements OnInit{
     console.log('summing data===>',d1 + d2)
     console.log('is same ',var1 === let1)
 
+    // //Data Types
+    // 1) String
+    // 2) Object
+    // 3) Number
+    // 4) Boolean
+    // 5) array
+    // 6) null
+    // 7) undefined
+    // 8) function
+    let temp
+    temp = 'vinay'
+    temp = 'vinay1'
+    
+    let numTemp = 3;
+    let boolTemp = true
+    const objTemp = {
+      brandName: 'ACER',
+      assembledIn: 'India',
+      yearMang: 2009,
+      color: 'black',
+      isMAC:false
+    }
+    let nullTemp = null;
+    let undefinedTemp;
+    const arrayTemp = [1,2,3,4,'',null,undefined, [], objTemp,boolTemp]
+    let funValue = checkDataType(10, 9, arrayTemp)
+    console.log('funValue',funValue)
+    function checkDataType(a, b, c) {
+      console.log('im in function')
+      return 3+3
+    }
+    
+    // const checkDataType1 = () =>{
+    //   console.log('im in function2')
+
+    // }
+
+    // ?? const
+    // hoisting
+
   }
 }

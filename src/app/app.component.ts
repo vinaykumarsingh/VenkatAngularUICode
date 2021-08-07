c8539d5d5cbcfd9297001ebf0fa4928b5d9fd803
vinaykumarsingh <vs3486@gmail.com>
2021-08-07 09:46:03 +0530

    string basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { concat } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,6 @@ export class AppComponent implements OnInit{
   }
 
   ngOnInit() {
-    debugger
     var var1 = 'vinay'
     let let1 = 'singh'
     let1 = 'vinay'
@@ -67,8 +67,56 @@ export class AppComponent implements OnInit{
 
     // }
 
-    // ?? const
-    // hoisting
+    // String and methods
+    // 1) length
+    // 2) slice
+    // 2) subString
+    // 3) Substr
+
+    ////Length
+    // let tempStr = 'vinaykumarsingh'
+    // let temp1 ;
+    // console.log('length', tempStr.length)
+    // temp1 = tempStr.length?'true':'false'
+    // console.log('length', temp1)
+
+    ////Slice
+    // let tempStr = 'vinaykumarsingh'
+    // let temp1 ;
+
+    // temp1 = tempStr.slice(5, tempStr.length)
+    // console.log('slice', temp1)
+    // let firstName =  tempStr.slice(0, 5)
+    // let middleName =  tempStr.slice(5, 10)
+    // let lastName =  tempStr.slice(10, tempStr.length)
+    // let fullName = firstName + " " + middleName  + " " + lastName
+    // console.log('fullName', fullName)
+
+    // tempStr.slice(-5)
+    // tempStr.slice(-5, tempStr.length)
+
+    ////subString (doesn't expect -ve value)
+    // let tempStr = 'vinaykumarsingh'
+    // let temp1 ;
+    // tempStr.substring(5)
+
+    ////Substr 
+    let tempStr = 'vinaykumarsingh'
+    let temp1 ;
+    temp1 = tempStr.substr(3,10)
+
+    // concat
+    tempStr.concat("ranchi")
+
+    //uppercase
+    tempStr.toUpperCase()
+    //trim
+    let x = '  vs3486@gmail.com     '
+    x.trim() === 'vs3486@gmail.com'
+
+    //string to array
+    var vv = 'vinay singh'
+    vv.split(" ")
 
   }
 }

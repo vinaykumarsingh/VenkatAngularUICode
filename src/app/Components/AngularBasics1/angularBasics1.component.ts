b316ac5152ba247b81b91329d8f1afbd231ad8fd
vinaykumarsingh <vs3486@gmail.com>
2021-08-25 09:34:29 +0530

    angular componnet lifecycle

--- src/app/Components/AngularBasics1/angularBasics1.component.ts
+++ src/app/Components/AngularBasics1/angularBasics1.component.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { concat } from 'rxjs';
 @Component({
   selector: 'app-angularBasics1',
   templateUrl: './angularBasics1.component.html',
   styleUrls: ['./angularBasics1.component.css']
 })
-export class AppAngularBasics1 implements OnInit{
+export class AppAngularBasics1 implements OnInit, OnDestroy{
 
   title = "Hi, Welcome1"
   a: number = 0.259;
 
   constructor() {
+    console.log("I'm in constructor")
+    
+  }
+    // ngOnChanges
+
+  // ngOnInit
+
+  // ngAfterViewInit
+
+  //ngOnDistroy
+  ngOnChanges(changes: SimpleChanges){
+    console.log("I'm in this.ngOnChanges")
+    console.log(changes)
+
   }
 
-  ngOnInit() {
 
+
+  ngOnInit() {
+    console.log("I'm in this.ngOnInit")
+    this.title = "Hi, Title chaznged"
+  }
+  
+  ngAfterViewInit() {
+    console.log("I'm in this.ngAfterViewInit")
+
+  }
+
+  ngOnDestroy(){
+    console.log("destroying child...")
   }
 
 }

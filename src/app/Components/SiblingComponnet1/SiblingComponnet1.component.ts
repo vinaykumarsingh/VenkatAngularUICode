bcea33ac3f1dda4e8e20f95fff57db8880976da3
vinaykumarsingh <vs3486@gmail.com>
2021-09-21 09:08:14 +0530

    final submission

--- src/app/Components/SiblingComponnet1/SiblingComponnet1.component.ts
+++ src/app/Components/SiblingComponnet1/SiblingComponnet1.component.ts
@@ -1,54 +1,63 @@
 import { Component, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { concat } from 'rxjs';
 import { ShareDataService } from 'src/app/Service/shareData.service';
-import {AfterViewInit, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-siblingComponnet1',
   templateUrl: './siblingComponnet1.component.html',
   styleUrls: ['./siblingComponnet1.component.css']
 })
-export class AppSiblingComponnet1 implements OnInit{
-  displayedColumns: string[] = ['id', 'name', 'phone', 'username', 'website','email', 'company', 'address'];
+export class AppSiblingComponnet1 implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'phone', 'username', 'website', 'email', 'company', 'address'];
   // displayedColumns: string[] = ['id', 'name', 'phone', 'username', 'website', 'email'];
-  
-  dataSource =[]
+
+  dataSource = []
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  
-  
-  datafromSharedService:any;
-  arrayData = ['a', [], {}, true, ]
-  isActive1= false;
-  isActive2 = false;
-  isValid =true;
-  isShowTable = false ;
-  
 
-  constructor(private shareDataService :ShareDataService) {
-  
+
+  datafromSharedService: any;
+  arrayData = ['a', [], {}, true,]
+  isActive1 = false;
+  isActive2 = false;
+  isValid = true;
+  isShowTable = false;
+
+
+  constructor(private shareDataService: ShareDataService) {
+
   }
 
 
   ngOnInit() {
   }
-  getDataFromService() {
-    debugger;
-    this.shareDataService.getCourses().subscribe((data)=>{
-      debugger;
-      this.datafromSharedService = data
-      this.isActive2= true
-      this.isValid =false;
-      this.dataSource = [...data]
-      console.log("this.dataSource=====>", this.dataSource)
-      this.isShowTable = true;
-    });
+
+  hideTable() {
+    this.isShowTable = false;
   }
-testMergeConflict(){
-  return false;
-}
+
+  getDataFromService() {
+
+    if (this.dataSource.length === 0) {
+      this.shareDataService.getCourses().subscribe((data) => {
+        debugger;
+        this.datafromSharedService = data
+        this.isActive2 = true
+        this.isValid = false;
+        this.dataSource = [...data]
+        this.isShowTable = true;
+      });
+    } else {
+      this.isShowTable = true;
+    }
+
+  }
+  testMergeConflict() {
+    return false;
+  }
 }
 
 
